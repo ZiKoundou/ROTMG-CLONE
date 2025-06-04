e1f80c8fe500329d1b483a19c168d8206fcc94d0
Zion Koundou <114265782+ZiKoundou@users.noreply.github.com>
2025-06-04 08:06:08 -0700

    added animations through blend tree

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,10 +5,16 @@ public class Movement : MonoBehaviour
 {
 
     [SerializeField] float speed = 5f;
-
+    private float animationSpeed;
     private Vector2 move;
     private SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
@@ -33,6 +39,20 @@ public class Movement : MonoBehaviour
             spriteRenderer.flipX = false;
         }
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        bool isIdle = movement.x == 0 && movement.z == 0;
+        if (isIdle)
+        {
+            animator.SetBool("isMoving", false);
+        }
+        else
+        {
+            animationSpeed = speed * 0.35f;
+            animator.SetFloat("moveSpeed", animationSpeed);
+            animator.SetFloat("horizontalMovement", movement.x);
+            animator.SetFloat("verticalMovement", movement.z);
+            animator.SetBool("isMoving", true);
+        }
+        
     }
 
     
